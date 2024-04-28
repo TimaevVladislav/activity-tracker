6d04d6848fe43ba68643e8f45c9b5244452bf4f9
timaevV <timaevvladislav@gmail.com>
2024-04-28 16:13:39 +0600

    feat: extension file middleware

---
+++ middlewares/extension.middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import path from "path";
+import fs from "fs";
+
+export const extensionMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (path.extname(req.path).length > 0) {
+    return next();
+  }
+
+  const file = path.join(process.cwd(), "./dist/src", `${req.path}.js`);
+  fs.access(file, fs.constants.F_OK, (err) => {
+    if (!err) {
+      req.url += ".js";
+    }
+
+    next();
+  });
+};
