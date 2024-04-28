a23bd9646bf3c6300ea3aeda4df06e3e536ad9bc
timaevV <timaevvladislav@gmail.com>
2024-04-28 16:14:59 +0600

    feat: track model

---
+++ models/track.schema.ts
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const TrackSchema = new mongoose.Schema(
+  {
+    event: { type: String, required: true },
+    tags: { type: Array, required: true },
+    url: { type: String, required: true },
+    title: { type: String, required: true },
+    ts: { type: Number, required: true },
+  },
+  { strict: true },
+);
+
+export default TrackSchema;
