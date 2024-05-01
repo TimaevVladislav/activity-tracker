caff28799eb0c7bd1a89e2dfcb35fab99b78a243
timaevV <timaevvladislav@gmail.com>
2024-05-02 03:22:12 +0600

    feat: prettier config

--- middlewares/extension.middleware.ts
+++ middlewares/extension.middleware.ts
@@ -1,22 +1,18 @@
-import { Request, Response, NextFunction } from "express";
-import path from "path";
-import fs from "fs";
+import { Request, Response, NextFunction } from "express"
+import path from "path"
+import fs from "fs"
 
-export const extensionMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+export const extensionMiddleware = (req: Request, res: Response, next: NextFunction) => {
   if (path.extname(req.path).length > 0) {
-    return next();
+    return next()
   }
 
-  const file = path.join(process.cwd(), "./dist/src", `${req.path}.js`);
+  const file = path.join(process.cwd(), "./dist/src", `${req.path}.js`)
   fs.access(file, fs.constants.F_OK, (err) => {
     if (!err) {
-      req.url += ".js";
+      req.url += ".js"
     }
 
-    next();
-  });
-};
+    next()
+  })
+}
